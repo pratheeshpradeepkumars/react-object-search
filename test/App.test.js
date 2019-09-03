fe6e0ed2f776d90a60bcfc8d805d365e4861c4ae
pratheesh.p <pratheesh.p@appviewx.com>
2019-09-03 16:05:33 +0530

    Jest doesn't works with JSX which imports CSS fix

--- test/App.test.js
+++ test/App.test.js
@@ -8,7 +8,7 @@ describe('App', () => {
     test('should match snapshot', () => {
         const wrapper = shallow(<App />);
 
-        expect(wrapper.find('h1').text()).toBe('Welcome to React Starter');
+        expect(wrapper.find('h2').first().text()).toBe('Object search filter using mutiple key values');
         expect(wrapper).toMatchSnapshot;
     });
 });
